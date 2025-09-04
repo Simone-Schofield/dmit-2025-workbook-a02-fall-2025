10f4ad2c086bc328339c8390813b69887c297828
Simone-Schofield <Simonealexysschofield@hotmail.com>
2025-09-04 14:28:16 -0600

    Class 1 & Kinda Class 2
    Had to redo the day 1 stuff and added the stuff i had from day 2 from the substitute. Need to get the rest of the stuff from brightspace and make sure it's updated.

--- module-1/02-php-basics/index.php
+++ module-1/02-php-basics/index.php
@@ -1,13 +1,18 @@
 <?php
+    /**
+     * In order for PHP to work, we need three things:
+     * 
+     * 1. a php-enable server (or environment);
+     * 2. a .php file; and
+     * 3. PHP code between <?php?> tags.
+     */
 
-/**
- * In order for PHP to work, we need three things:
- * 
- * 1. a PHP-enabled server (or environment);
- * 2. a .php file; and
- * 3. PHP code between <?php?> tags.
- */
-
+    /**
+     * Starting up the Terminal
+     * Just put => docker compose up -d <= and it will start up.
+     * To check the website => http://localhost:8080/dmit-2025-workbook/
+     * Just put => docker compose down <= and this will turn it off.
+     */
 ?>
 
 <!doctype html>
@@ -15,15 +20,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>PHP Basics</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body class="container text-center">
     <section class="row min-vh-100 align-items-center justify-content-center">
         <div class="col-lg-8">
-            <h1 class="diplay-5 mb-4">PHP Basics</h1>
+            <h1 class="display-5 mb-4">PHP Basics</h1>
             <h2 class="fw-light">Table of Contents</h2>
 
             <ul class="nav flex-column">
@@ -38,7 +41,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="arrays.php" class="nav-link">Insult Generator</a>
-                </li>
+                </li>                
             </ul>
         </div>
     </section>
